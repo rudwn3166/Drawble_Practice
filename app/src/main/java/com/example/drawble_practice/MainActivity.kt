9406b56d0633de0a824367962170723c58a88522
Joo <rudwn3166@naver.com>
2021-10-29 19:39:33 +0900

    양 옆의 여백 공간을 제거함

--- app/src/main/java/com/example/drawble_practice/MainActivity.kt
+++ app/src/main/java/com/example/drawble_practice/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.drawble_practice
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.appcompat.app.ActionBar
+import androidx.appcompat.widget.Toolbar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,5 +21,14 @@ class MainActivity : AppCompatActivity() {
 //        실제로 어떤 커스텀 뷰를 사용할지 ?
 //        여기까지하면 액션바가 바뀌긴 하나 양옆의 여백이 생김
         defaultActionbar.setCustomView(R.layout.my_cutsom_action_bar)
+
+//        양 옆여백은 커스텀 바가 아니라서 커스텀 바를 가지고 있는 toolbar를 찾아 여백 제거하기
+//        커스텀 바를 가지고 있는 툴바를 불러내기
+
+//        as캐스팅을 사용해 툴바를 커스텀 바로 바꿈
+        val toolbar =defaultActionbar.customView.parent as Toolbar
+        
+//        양 옆의 여백을 모두 무조걱 0으로 없앤다고 함
+        toolbar.setContentInsetsAbsolute(0,0)
     }
 }
